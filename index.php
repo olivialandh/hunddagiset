696dc851c03027ec71a79e151c27b75ec3fe5a0e
olivialandh <olivialandh@hotmail.com>
2014-03-19 11:48:51 +0100

    ge
    ge

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+<wrapper>
+    <header>
+
+    </header>
+    <nav>
+        <a href="index.php">Startsida</a>
+        <a href="news.php">Nyheter</a>
+        <a href="about.php">Om oss</a>
+        <a href="info.php">Priser</a>
+        <a href="gallery.php">Galleri</a>
+        <a href="contact.php">Kontakt</a>
+    </nav>
+    <content>
+        <!--bild som skiftar varje gång sidan laddas om-->
+        <img src="#"/>
+        <div id="welcometextbox">
+            <p>
+                Välkommen till det personliga hunddagiset i Uddavalla. Vi erbjuder dagis i hemmiljö och gör allt för att du och just din eller dina hundar ska ha det så roligt, bekvämt och tryggt som möjligt.
+            </p>
+        </div>
+    </content>
+    <footer>
+        <ul>
+            <li>
+                <img src="#"/>
+                <p>
+                    076-9484263
+                </p>
+            </li>
+            <li>
+                <img src="#"/>
+                <p>
+                    brittalisashunddagis@telia.com
+                </p>
+            </li>
+            <li>
+                <img src="#"/>
+                <p>
+                    Hova Solbacken 125<br>451 91 Uddevalla
+                </p>
+            </li>
+        </ul>
+    </footer>
+</wrapper>
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+</script>
+</body>
+</html>
